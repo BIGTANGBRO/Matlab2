4b3b7850294064ecada1f5ed527f7f7e8efdb45c
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-02-14 18:59:23 +0000

    34
    1

--- term2/t13_1.m
+++ term2/t13_1.m
@@ -2,17 +2,37 @@ clc
 clear
 fid1=fopen('grades.dat','r');
 num=fscanf(fid1,'%d',1);
-for j=1:28
+
+for j=1:29
     for i=1:13
         data(j,i)={fscanf(fid1,'%s',1)};
     end
 end
-structure.name=data([2:28],1);
-structure.surname=data([2:28],2);
-structure.alias=data([2:28],3);
-structure.grades=data([2:28],[4:13]);
-classes={'MathsI',data(2:28,4),'MathsII',data(2:28,5),'PhysicsI',data(2:28,6),'PhysicsII',data(2:28,7),'Chemistry',data(2:28,8),'Econometrics',data(2:28,9),'EnglishLit',data(2:28,10),'Ancient_Greek',data(2:28,11),'PhysED',data(2:28,12),'Tok',data(2:28,13)};
-for k =1:27
-    k=fopen('alias(k).txt','w');
-    fprintf(k,'%1s   %2s     %3$4.1f',structure.name(i),structure.surname(i),structure.grades(i,:));
+fclose(fid1);
+for i=1:28
+    structure(i).name=data(i+1,1);
+    structure(i).surname=data(i+1,2);
+    structure(i).alias=data(i+1,3);
+    structure(i).grades=data(i+1,[4:13]);
 end
+for j=1:28
+    Student(j).grades=str2double(structure(j).grades);
+end
+
+classes=data(1,4:13);
+mkdir marks_3
+cd marks_3
+
+for k =1:28
+    fname=[char(structure(k).alias),'-report.txt'];
+    fid=fopen(fname,'w');
+    fprintf(fid,'Student report card: %s \n',date);
+    fprintf(fid,'Student name: %s %s \n\n',char(structure(k).name),char(structure(k).surname));
+    fprintf(fid,'Grades \t\t Subject \n');
+    for j=1:10
+        fprintf(fid,'%3s \t\t %s \n',char(structure(k).grades(j)),char(classes(j)));
+    end
+    fprintf(fid,'Average: \t  %5.2f \n',mean(Student(k).grades));
+    fclose(fid);
+end
+cd ..
