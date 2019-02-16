37d499fc5108eb517f4c1d23185f2cd5472157dc
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-02-16 10:12:42 +0000

    Update t_13_sol_1.m

--- term2/t_13_sol_1.m
+++ term2/t_13_sol_1.m
@@ -5,37 +5,40 @@ clc
 fid=fopen('grades.dat','r');
 %read number of students from first line
 n=fscanf(fid,'%d \n',1);
-fscanf(fid,'%*s',3); %skip next 3 strings
+fscanf(fid,'%*s',3);%skip next 3 strings
 %use loop to read class names into cell array
 for i=1:10
 classes(i)={fscanf(fid,'%s',1)};
 end
 %read the marks table row by row
 for i=1:n
-record(i).name=fscanf(fid,'%s',1);
-record(i).surname=fscanf(fid,'%s',1);
-record(i).alias=fscanf(fid,'%s',1);
-record(i).grades=fscanf(fid,'%d',10); %read into 10x1 array
+    record(i).name=fscanf(fid,'%s',1);
+    record(i).surname=fscanf(fid,'%s',1);
+    record(i).alias=fscanf(fid,'%s',1);
+    record(i).grades=fscanf(fid,'%d',10); %read into 10x1 array
 end
 fclose(fid); %close file
+
 %create folder for marks and change directory
 mkdir marks
 cd marks
+
+
 for i=1:n
 %create filename based on student alias
-fname=[record(i).alias,'-report.txt'];
+    fname=[record(i).alias,'-report.txt'];
 %open file and give overwrite permission
-fid=fopen(fname,'w');
+    fid=fopen(fname,'w');
 %report title and creation date
-fprintf(fid,'\t\t STUDENT REPORT CARD: %s \n\n',date);
+    fprintf(fid,'\t\t STUDENT REPORT CARD: %s \n\n',date);
 %student name
-fprintf(fid,'Student: %s %s \n\n',record(i).name,record(i).surname);
+    fprintf(fid,'Student: %s %s \n\n',record(i).name,record(i).surname);
 %create header for results
-fprintf(fid,'Grade \t\t Subject \n');
+    fprintf(fid,'Grade \t\t Subject \n');
 %print marks and course names
-for j=1:10
-fprintf(fid,'%3d \t\t %s \n',record(i).grades(j),char(classes(j)));
-end
+    for j=1:10
+        fprintf(fid,'%3d \t\t %s \n',record(i).grades(j),char(classes(j)));
+    end
 %calculate and print mean
 fprintf(fid,'\n%5.2f \t\t Average\n',mean(record(i).grades));
 fclose(fid); %close file
