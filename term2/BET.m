7584abbffe2eb7555f0ffd7365b33b4f71887780
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-03-30 19:05:53 +0000

    1

--- term2/BET.m
+++ term2/BET.m
@@ -1,74 +1,167 @@
-clc
+%Script that approximates the side and drag forces, the average rotor 
+%torque acting on a rotor disk, and the power requirement to drive the
+%rotor for a rotor disk of user-specified geometry travelling at a 
+%user-specified air velocity with vertical and forward components.
+%Written by Jonathan De Sousa
+%Credit to Dr Errikos Levis for cubicEval.m function
+%Date: 23-03-2019
+
+%Clear workspace and screen and close all open figures
 clear
+clc
+close all
 
-fid=fopen('N0012.dat','r');
-trash=fscanf(fid,'%*s',3);
-for i=1:68
-    for j=1:3
-        AOA_VS_CL_CD(i,j)=fscanf(fid,'%e',1);
+%Prompt user to input operating conditions
+disp('OPERATING CONDITIONS')    
+omega=input('Input rotor angular velocity (rad/s): ');
+i_c=input('Input collective blade setting angle (degrees): ');
+V_inf=input('Input forward airspeed (m/s): ');
+W_inf=input('Input rate of descent (m/s): ');
+rho=input('Input air density (kg/m^3): ');
+
+clc %clear screen
+
+%Prompt user to define rotor geometry
+disp('ROTOR GEOMETRY')
+N=input('Input number of blades: ');
+D=input('Input rotor diameter (m): ');
+R_0=input('Input blade starting radius (m): ');
+c_root=input('Input root chord length (m): ');
+c_tip=input('Input tip chord length (m): ');
+epsilon=input('Input blade twist angle from root to tip (degrees): ');
+
+%Change user-input angles into radians
+i_c=pi*i_c/180;
+epsilon=pi*epsilon/180;
+
+clc %clear screen
+
+%Prompt user for full file name of ASCII file containing lift and drag 
+%coefficients at different angles of attack.
+filename=input('Input full name of file containing aerodynamic characteristics of airfoil: ','s');
+
+
+%Load file
+fID=fopen(char(filename),'r');
+
+%Read file
+fscanf(fID,'%s',3); %skip first three lines (table headings)
+data=fscanf(fID,'%f',[3,inf]); %extract data from file
+
+%Transpose data array and separate data into categories
+data=data';
+AoA=data(:,1); %angles of attack
+CL=data(:,2); %corresponding coefficients of lift
+CD=data(:,3); %corresponding coefficients of drag
+
+%Discretize rotor blade 
+m_R=100; %number of discrete rotor sections to carry out calculations
+R=(R_0:((D/2-R_0)/(m_R-1)):D/2); %distances of each section from rotor disc centre (m)
+%Calculate local chord for each discretized section using linear
+%interpolation
+c_local=((c_tip-c_root)/(D/2-R_0)).*(R-R_0)+c_root;
+
+%Set constants
+Fn_old=0; %initial thrust force of rotor disc (N)
+m_psi=360; %number of discretized azimuth angles
+psi=(0:2*pi/(m_psi-1):2*pi); %azimuth angles of the blade (rad)
+diff=99; %difference between successive calculated total thrust values
+
+%Pre-allocate arrays
+V_e=zeros(m_psi,m_R);
+delta_alpha=zeros(m_psi,m_R);
+CL_InterpVal=zeros(m_psi,m_R);
+CD_InterpVal=zeros(m_psi,m_R);
+
+%Turn c_local, psi and R arrays into square arrays
+c_local=c_local.*ones(m_psi,m_R);
+
+psi2=psi;
+R2=R;
+for count = 1:m_R-1
+    psi=[psi;psi2];
+    R=[R;R2];
+end
+
+%Loop iterative calculation of total thrust force, Fn, until Fn converges
+while diff>eps %eps is computational precision
+    for i=1:m_psi
+        for j=1:m_R
+            %Calculate total air velocity normal to the rotor plane (m/s)
+            if W_inf<=0
+                W=(W_inf/2)-0.5*sqrt(W_inf^2+8*Fn_old/(rho*pi*D^2));
+            elseif W_inf>sqrt(8*Fn_old/(rho*pi*D^2))
+                W=(W_inf/2)+0.5*sqrt(W_inf^2-8*Fn_old/(rho*pi*D^2));
+            else
+                %Display warning since rotorcraft is in Vortex Ring State
+                disp('WARNING: ROTORCRAFT IS IN DANGEROUS VORTEX RING STATE')
+                break %break loop
+            end
+            %Calculate tangential velocity to blade in rotor disc plane (m/s)
+            V_T=omega*R(j)+V_inf*sin(psi(i));
+            %Calculate local airspeed in plane of blade element (m/s)
+            V_e(i,j)=sqrt(V_T^2+W^2);
+            %Calculate local rotation of the airflow relative to the plane of
+            %the disk (rad)
+            delta_alpha(i,j)=atan(W/V_T);
+            %Calculate angle of attack (rad) of blade element
+            alpha_e=i_c+((R(j)-R_0)*epsilon/(D/2-R_0))+delta_alpha;
+            %Use cubic spline interpolation to find coefficients of drag and
+            %lift at each section using cubicEval.m and coeff.m functions used
+            %in Cubic Spline Interpolation question
+            polycoeff=coeff(AoA,CL); %calculate polynomial coefficients of interpolant
+            CL_InterpVal(i,j)=cubicEval(AoA,polycoeff,alpha_e); %interpolated CL value
+            polycoeff=coeff(AoA,CD); %calculate polynomial coefficients of interpolant
+            CD_InterpVal(i,j)=cubicEval(AoA,polycoeff,alpha_e); %interpolated CD value
+        end
     end
-end
-
-N=4;%blade numbers
-D=16.36;%diameter of the blade
-R0=1.55;
-Croot=0.53;
-Ctip=0.53;
-E=-18;%degree linear blade twist
-angular_velocity=27;%rads s^-1
-ic=input('input the blade setting angle');%get the blade setting angle
-Vinf=input('input the forward airspeed');%forwad speed
-Winf=input('input the rate of descent');
-twist=input('input the twist of the blade');
-density=1.12;
-cut=200;
-
-for i=1:200
-    section_chord(i)=Croot+(Ctip-Croot)*i/200;%the width for each small pieces
-    R(i)=R0+(D/2-R0)*i/200;%Distance from center, value of R
-end
-FN=0;
-azimuth_angle=0;
-
-%calculate the velocity normal to disc W
-%need condition to do calculation
-if Winf<=0
-    W=Winf/2-1/2*sqrt(Winf^2+8*FN/(pi*density*D^2));
-elseif Winf >sqrt(8*FN/(density*pi*D^2))
-    W=Winf/2+1/2*sqrt(Winf^2-8*FN/(pi*density*D^2));
-else
-    disp('No analytical solution.');
-end
-
-for i=1:200
-    VT(i)=angular_velocity*R(i)+Vinf*sin(azimuth_angle);%to calculate tangential velocity
-    Ve(i)=(VT(i)^2+W^2)^0.5;%to calculate downward velocity;
-    deltaA=atan(W/VT(i));
-    ae(i)=ic+(R(i)-R0)/(D/2-R0)+deltaA;%to calculate angle of attack
-end
-
-% use cubic spline to calculate corresponding value of CL and CD specific
-% angle of attack
-CL=[];%initiate lift coefficient
-CD=[];%initiate dray coefficient
-AOA_list=AOA_VS_CL_CD(:,1);
-CL_list=AOA_VS_CL_CD(:,2);
-CD_list=AOA_VS_CL_CD(:,3);
-polyArray_CL=CubicIn(AOA_list,CL_list);
-polyArray_CD=CubicIn(AOA_list,CD_list);
-%still need to use nested loop to calculate CL and CD for different
-%azimuth_angle.
-
-for i=1:200
-    CL(i)=CubicEval(AOA_list,polyArray_CL,ae(i));
-    CD(i)=CubicEval(AOA_list,polyArray_CD,ae(i));%use function created before to find corresponding CD and CL
-end
-
-%use trapz function of calculate double integrals
-
-
-
     
+    %Calculate total thrust force Fn_new of the rotor disc
+    dFn=0.5*rho*V_e.^2.*c_local.*(CL_InterpVal.*cos(delta_alpha)+CD_InterpVal.*sin(delta_alpha));
+    Fn_new=(N/(2*pi))*trapz(psi,trapz(R,dFn));
+    
+    %Calculate difference between successive thrust force values
+    diff=abs(Fn_new-Fn_old);
+    Fn_old=Fn_new;
+end
+
+%Calculate drag force
+dFx=0.5*rho*V_e.^2.*c_local.*(CD_InterpVal.*cos(delta_alpha)-CL_InterpVal.*sin(delta_alpha)).*sin(psi);
+Fx=(N/(2*pi))*trapz(psi,trapz(R,dFx));
+
+%Calculate side force
+dFy=0.5*rho*V_e.^2.*c_local.*(CD_InterpVal.*cos(delta_alpha)-CL_InterpVal.*sin(delta_alpha)).*cos(psi);
+Fy=-(N/(2*pi))*trapz(psi,trapz(R,dFy));
+
+%Calculate the average torque over the rotor
+dT=0.5*rho*V_e.^2.*c_local.*(CD_InterpVal.*cos(delta_alpha)-CL_InterpVal.*sin(delta_alpha)).*R;
+T=(N/(2*pi))*trapz(psi,trapz(R,dT));
+
+P=T*omega; %power P required to drive the rotor
+
+%Calculate the pitching 
+dMx=0.5*rho*V_e.^2.*c_local.*(CL_InterpVal.*cos(delta_alpha)+CD_InterpVal.*sin(delta_alpha)).*R.*cos(psi);
+Mx=-(N/(2*pi))*trapz(psi,trapz(R,dMx));
+
+%Calculate the rolling moment
+dMy=0.5*rho*V_e.^2.*c_local.*(CL_InterpVal.*cos(delta_alpha)+CD_InterpVal.*sin(delta_alpha)).*R.*sin(psi);
+My=-(N/(2*pi))*trapz(psi,trapz(R,dMy));
+
+%Display results
+disp(['Total thrust force: ',num2str(Fn),' N']);
+disp(['Drag force: ',num2str(Fx),' N']);
+disp(['Side force: ',num2str(Fy),' N']);
+disp(['Rolling moment: ',num2str(My),' Nm']);
+disp(['Pitching moment: ',num2str(Mx),' Nm']);
+disp(['Power to drive rotor: ',num2str(P),' W']);
+
+
+        
+        
+        
+        
+        
+
 
 
 
--- term2/BET_edit.m
+++ term2/BET_edit.m
@@ -2,7 +2,6 @@
 %splitting blade into pieces
 %all the inputs about angles should be in radians except twist(degree)
 %written by Jiaxuan Tang for Computing coursework quesion 2
-
 clc
 clear
 
@@ -20,7 +19,7 @@ end
 %degree so when calculating angle of attack below, you should also use
 %degree
 
-
+%set the initial condition
 N=4;%blade numbers
 D=16.36;%diameter of the blade
 R0=1.55;
@@ -29,35 +28,23 @@ Ctip=0.53;
 E=-18;%degree linear blade twist
 angular_velocity=27;%rads s^-1
 density=1.12;%density of air
-cut=200;
-azimuth_angle=0:pi/100:(2*pi-pi/100);
+R_cut=200;
+psi_cut=360;
+azimuth_angle=0:pi/100:2*pi;
 
-ic=input('input the blade setting angle');%get the blade setting angle input will be radian
-Vinf=input('input the forward airspeed');%forwad speed
-Winf=input('input the rate of descent');
-twist=input('input the twist of the blade')*2*pi/360;%input will be degree so turn in to radian
-
-
+ic=input('input the blade setting angle(degree):')*pi/180;%get the blade setting angle,turn it into radian
+Vinf=input('input the forward airspeed(ft/s):');%forwad speed
+Winf=input('input the rate of descent(ft/s):');
+twist=E*pi/180;%input will be degree so turn in to radian
 
 %caluculate the local chord for each small section and the positions of
 %them
-for i=1:200
+for i=1:R_cut
     section_chord(i)=Croot+(Ctip-Croot)*i/200;%the width for each small pieces
     R(i)=R0+(D/2-R0)*i/200;%Distance from center, value of R
 end
-FN=0;
-
-
-%calculate the velocity normal to disc W
-%need condition to do calculation
-if Winf<=0
-    W=Winf/2-1/2*sqrt(Winf^2+8*FN/(pi*density*D^2));
-elseif Winf >sqrt(8*FN/(density*pi*D^2))
-    W=Winf/2+1/2*sqrt(Winf^2-8*FN/(pi*density*D^2));
-else
-    disp('No analytical solution.');
-end
-
+Fn_init=0;%set the inital totol thrust
+psi=0:2*pi/(psi_cut-1):2*pi;%azimuth_angle
 
 % use cubic spline to calculate corresponding value of CL and CD specific
 % angle of attack
@@ -68,51 +55,66 @@ CL_list=AOA_VS_CL_CD(:,2);
 CD_list=AOA_VS_CL_CD(:,3);
 polyArray_CL=CubicIn(AOA_list,CL_list);
 polyArray_CD=CubicIn(AOA_list,CD_list);
-%still need to use nested loop to calculate CL and CD for different
-%azimuth_angle.
 
+%use meshgrid to turn 1D array to matrix(R,section_chord,azimuth_angle(psi)
+[~,section_chord]=meshgrid(psi,section_chord);
+[psi,R]=meshgrid(psi,R);
 
-%this nested loop is to create 200x200 array as both the blade section and
-%the azimuth angle are divided into 200 pieces and 
-%i represents sections and j represents azimuth angle so one row gets
-%value for differnet sections but same azimuth angle and one column
-%represents different azimuth angles but same section.
-for i=1:200
-    for j=1:200
-        VT(i,j)=angular_velocity*R(j)+Vinf*sin(azimuth_angle(i));%to calculate tangential velocity
-        Ve(i,j)=(VT(i,j)^2+W^2)^0.5;%to calculate downward velocity;
-        deltaA(i,j)=atan(W./VT(i,j));
-        ae(i,j)=(ic+(R(j)-R0)*twist/(D/2-R0)+deltaA(i,j))*360/(2*pi);%to calculate angle of attack(use degree)
-        CL(i,j)=cubicEval(AOA_list,polyArray_CL,ae(i,j));
-        CD(i,j)=cubicEval(AOA_list,polyArray_CD,ae(i,j));%use function created before to find corresponding CD and CL
+D=5;%set the initial difference
+while D>0.0001
+    %this nested loop is to create 200x360 array as both the blade section are divided by R_cut and
+    %the azimuth angle are divided into psi_cut pieces 
+    %i represents sections and j represents azimuth angle so one row gets
+    %value for differnet sections but same azimuth angle and one column
+    %represents different azimuth angles but same section.
+    %calculate the velocity normal to disc W
+    %need condition to do calculation
+    if Winf<=0
+        W=Winf/2-1/2*sqrt(Winf^2+8*Fn_init/(pi*density*D^2));
+    elseif Winf >sqrt(8*Fn_init/(density*pi*D^2))
+        W=Winf/2+1/2*sqrt(Winf^2-8*Fn_init/(pi*density*D^2));
+    else
+        disp('No analytical solution.');
+        break
     end
+    for i=1:R_cut
+        for j=1:psi_cut
+           VT(i,j)=angular_velocity*R(i,j)+Vinf*sin(psi(i,j));%to calculate tangential velocity
+           Ve(i,j)=(VT(i,j)^2+W^2)^0.5;%to calculate downward velocity;
+           deltaA(i,j)=atan(W./VT(i,j));
+           ae(i,j)=(ic+(R(i,j)-R0)*twist/(D/2-R0)+deltaA(i,j))*360/(2*pi);%to calculate angle of attack(use degree)
+           CL(i,j)=cubicEval(AOA_list,polyArray_CL,ae(i,j));
+           CD(i,j)=cubicEval(AOA_list,polyArray_CD,ae(i,j));%use function created before to find corresponding CD and CL
+           end
+    end
+     d_Fn=N*(0.5*density*Ve.^2).*section_chord.*(CL.*cos(deltaA)+CD.*sin(deltaA))./(2*pi);
+     Fn=trapz(psi(1,:),trapz(R(:,1),d_Fn));
+     D=abs(Fn-Fn_init);
+     Fn_init=Fn;
 end
-
 
 %use trapz function of calculate double integrals
 %calculate total thrust of the rotor disc
 %fitsr integrate by Radius then by psi
-d_Fn=N*(0.5*density*Ve.^2).*section_chord.*(CL.*cos(deltaA)+CD.*sin(deltaA))./(2*pi);
-Fn=trapz(azimuth_angle,trapz(R,d_Fn));
 
-d_Fx=N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*sin(azimuth_angle)./(2*pi);
-Fx=trapz(azimuth_angle,trapz(R,d_Fx));
+d_Fx=N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*sin(psi)./(2*pi);
+Fx=trapz(psi(1,:),trapz(R(:,1),d_Fx));
 
-d_Fy=-N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*cos(azimuth_angle)./(2*pi);
-Fy=trapz(azimuth_angle,trapz(R,d_Fy));
+d_Fy=-N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*cos(psi)./(2*pi);
+Fy=trapz(psi(1,:),trapz(R(:,1),d_Fy));
 
 d_T=N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*R./(2*pi);
-T=trapz(azimuth_angle,trapz(R,d_T));
+T=trapz(psi(1,:),trapz(R(:,1),d_T));
 
 %calculate diving power
 P=T*angular_velocity;
 
 %calculate pitching and rolling moments
-d_Mx=-N*(0.5*density*Ve.^2).*section_chord.*(CL.*cos(deltaA)+CD.*sin(deltaA)).*R.*cos(azimuth_angle)./(2*pi);
-Mx=trapz(azimuth_angle,trapz(R,d_Mx));
+d_Mx=-N*(0.5*density*Ve.^2).*section_chord.*(CL.*cos(deltaA)+CD.*sin(deltaA)).*R.*cos(psi)./(2*pi);
+Mx=trapz(psi(1,:),trapz(R(:,1),d_Mx));
 
-d_My=-N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*R.*sin(azimuth_angle)./(2*pi);
-My=trapz(azimuth_angle,trapz(R,d_My));
+d_My=-N*(0.5*density*Ve.^2).*section_chord.*(CD.*cos(deltaA)+CL.*sin(deltaA)).*R.*sin(psi)./(2*pi);
+My=trapz(psi(1,:),trapz(R(:,1),d_My));
 
 %all values are worked out
 
