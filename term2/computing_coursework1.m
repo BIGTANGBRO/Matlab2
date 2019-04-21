8cc849bac6c0252bba53af9e04257ec2bec5fced
jt2418 <tangjiaxuan_zjg@outlook.com>
2019-04-21 18:12:44 +0100

    Update computing_coursework1.m

--- term2/computing_coursework1.m
+++ term2/computing_coursework1.m
@@ -5,7 +5,14 @@ clear
 x=input('Input the array of x:');
 fx=input('Input the value of y:');
 xval=input('Input the value of x that you want of interpolate:');
-polyArray=CubicIn(x,fx);
-Y_value=cubicEval(x,polyArray,xval);
-%one output each time
-disp(Y_value);
+if length(xval)>1
+    print('The input of xval should be a scalar')
+elseif length(x)~=length(fx)
+    print('The coordiantes have different dimensions')
+else
+    polyArray=CubicIn(x,fx);%get the coefficients from function CubicIn
+    %xval must be a scalar
+    Y_value=cubicEval(x,polyArray,xval);
+    %one output each time
+    disp(Y_value);
+end
