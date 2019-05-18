0d74aeffc99de356f0cbe6ecc3a7ce41c398fca3
jt2418 <tangjiaxuan_zjg@outlook.com>
2019-05-19 00:03:45 +0100

    Update computing_coursework1.m

--- term2/computing_coursework1.m
+++ term2/computing_coursework1.m
@@ -1,7 +1,7 @@
 %the script that will use two functions to calculate corresponding value
 clc
 clear
-%ask users for the input
+%ask users for the input(in array)
 x=input('Input the array of x:');
 fx=input('Input the value of y:');
 xval=input('Input the value of x that you want of interpolate:');
