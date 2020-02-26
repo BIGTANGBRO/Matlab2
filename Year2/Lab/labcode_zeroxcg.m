1821c1c114ee93b4761c39aceb3352cfd7e6e762
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-26 20:02:28 +0000

    1

--- Year2/Lab/labcode_zeroxcg.m
+++ Year2/Lab/labcode_zeroxcg.m
@@ -10,8 +10,11 @@ WeightOfFuel = MassOfFuel.*9.81;
 CL = WeightOfFuel./(0.5.*rho.*IAS.^2.*17.48);
 AoA=[-0.6,-0.3,0.1,0.45,1.25,2.3];
 
-plot(AoA,CL);
+p1=polyfit(AoA,CL,1);
+scatter(AoA,CL);
+range = [-10:0.1:10];
 hold on
+plot(range,p1(1).*range + p1(2));
 %%
 %landing
 IAS_landing = [108.9,99.3,91,80.6,75.2,69.8]*0.5144;
@@ -21,6 +24,15 @@ WeightofFuel_landing = MassOfFuel_landing.*9.81;
 CL_landing = WeightofFuel_landing./(0.5.*rho.*IAS_landing.^2.*17.48);
 AoA_landing = [-4.3,-2.85,-1,2.1,3.6,5.7];
 
-plot(AoA_landing,CL_landing)
+hold on
+scatter(AoA_landing,CL_landing,'*');
+p2 = polyfit(AoA_landing,CL_landing,1);
+hold on
+plot(range,p2(1).*range+p2(2));
+legend('points for clean setup','line for clean setup','points for landing setup','line for landing setup');
+xlabel('Angel of attack in degree');
+ylabel('CL(Coefficient of life)');
+title('CL vs AoA for zero Xcg');
+
 
 
