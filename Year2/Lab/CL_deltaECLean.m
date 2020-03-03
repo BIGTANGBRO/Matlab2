be4c8afaa8349ea54312f465b3d101c518469096
ScarletRain <992127134@qq.com>
2020-03-03 15:37:04 +0000

    up

--- Year2/Lab/CL_deltaECLean.m
+++ Year2/Lab/CL_deltaECLean.m
@@ -1,8 +1,8 @@
-clc
 clear
+clc
 
 %%Zero xcg AOA vs CL
-%%clean
+%%clean 
 rho=1.225;
 IAS = [239.2,220.1,199.5,180.9,160.3,139]*0.5144;
 MassOfFuel=(3100+503+[980,976,971.8,968.6,965,959.4]).*0.453592;
@@ -10,18 +10,16 @@ WeightOfFuel = MassOfFuel.*9.81;
 CL = WeightOfFuel./(0.5.*rho.*IAS.^2.*17.48);
 AoA=[-0.6,-0.3,0.1,0.45,1.25,2.3];
 
-p1=polyfit(AoA,CL,1);
-scatter(AoA,CL);
-range = [-2:0.1:4];
+rangeE=[-5:0.01:2];
+deltaE=[0.04*14,0.02*14,-0.01*30,-0.025*30,-0.055*30,-0.095*30];
+scatter(deltaE,CL,'r*');
+pdeltaE = polyfit(deltaE,CL,1);
 hold on
-plot(range,p1(1).*range + p1(2));
-hold on 
-%%
-%for cg - 5
-%clean
-clear 
-clc
+plot(rangeE,pdeltaE(1).*rangeE+pdeltaE(2),'r');
+hold on
 
+%%
+%xcg-5
 IAS = [240,221,202,179.5,160.1,140.5].*0.5144;
 rho = 1.225;
 MassOfFuel=(3100+503+[971.6,969.3,965.1,961.4,956.1,953.9]).*0.453592;
@@ -29,15 +27,16 @@ WeightOfFuel = MassOfFuel.*9.81;
 CL = WeightOfFuel./(0.5.*rho.*IAS.^2.*17.48);
 AoA=[-0.55,-0.18,-0.15,-0.047,1.004,2.144];
 
-p1=polyfit(AoA,CL,1);
-scatter(AoA,CL);
-range = [-2:0.1:4];
+deltaE=[+0.01*14,-0.01*30,-0.02*30,-0.015*30,-0.068*30,-0.111*30];
+scatter(deltaE,CL,'g*');
+pdeltaE = polyfit(deltaE,CL,1);
 hold on
-plot(range,p1(1).*range + p1(2));
-hold on 
+plot(rangeE,pdeltaE(1).*rangeE+pdeltaE(2),'g');
+hold on
+
 %%
-%%xcg + 5AOA vs CL
-%clean
+%xcg+5
+
 rho=1.225;
 IAS = [240,221.25,203.8,180,158.9,142]*0.5144;
 MassOfFuel=(3100+503+[910.75,897,884.1,869.6,861.6,854]).*0.453592;
@@ -45,14 +44,14 @@ WeightOfFuel = MassOfFuel.*9.81;
 CL = WeightOfFuel./(0.5.*rho.*IAS.^2.*17.48);
 AoA=[-0.8,-0.5,-0.197,0.4365,1.214,2.026];
 
-p1=polyfit(AoA,CL,1);
-scatter(AoA,CL);
-range = [-2:0.1:4];
+deltaE=[0.07*14,0.055*14,0.03*14,-0.05767*30,-0.02962*30,-0.05614*30];
+scatter(deltaE,CL,'b*');
+pdeltaE = polyfit(deltaE,CL,1);
 hold on
-plot(range,p1(1).*range + p1(2));
+plot(rangeE,pdeltaE(1).*rangeE+pdeltaE(2),'b');
+hold on
 
-legend('points for zero Xcg','line for zero Xg','points for Xcg-5','line for Xcg-5','points for Xcg+5','line for Xcg+5');
-xlabel('Angel of attack in degree');
-ylabel('CL(Coefficient of life)');
-title('CL vs AoA for clean setup');
-hold off
+title('CL vs deltaE for clean setup')
+ylabel('CL')
+xlabel('Elevator deflection in angle');
+legend('Xcg=0 points','Line for Xcg=0','Xcg-5 points','Line for Xcg-5','Xcg+5 points','Line for Xcg+5');
---
+++ Year2/Lab/combine2.m
@@ -0,0 +1,58 @@
+
+%%
+clear
+clc
+%xcg=0
+%landing
+range = [-10:0.1:10];
+rho = 1.225;
+IAS_landing = ([108.9,99.3,91,80.6,75.2,69.8])*0.5144;
+MassOfFuel_landing = ([930,924.8,919,915.6,907.8,903.3]+3100+503).*0.453592;
+
+WeightofFuel_landing = MassOfFuel_landing.*9.81;
+CL_landing = WeightofFuel_landing./(0.5.*rho.*IAS_landing.^2.*17.48);
+AoA_landing = [-4.3,-2.85,-1,2.1,3.6,5.7];
+
+hold on
+scatter(AoA_landing,CL_landing,'*');
+p2 = polyfit(AoA_landing,CL_landing,1);
+hold on
+plot(range,p2(1).*range+p2(2));
+hold on
+%%
+%landing
+%xcg-5
+IAS_landing = [119.3,111.3,100.6,90.5,80.2,69.6].*0.5144;
+rho = 1.225;
+MassOfFuel_landing=(3100+503+[936.8,926.9,920.8,917.6,917,914.6]).*0.453592;
+WeightOfFuel_landing = MassOfFuel_landing.*9.81;
+CL_landing = WeightOfFuel_landing./(0.5.*rho.*IAS_landing.^2.*17.48);
+AoA_landing=[-5.248,-4.286,-3.312,-0.781,2.33,6.249];
+
+hold on
+scatter(AoA_landing,CL_landing,'*');
+p2 = polyfit(AoA_landing,CL_landing,1);
+hold on
+plot(range,p2(1).*range+p2(2));
+hold on 
+%%
+%landing
+%xcg + 5
+IAS_landing = ([119.4,109.7,99.13,90.9,80.6,69.72])*0.5144;
+MassOfFuel_landing = ([776.5,795.1,807,818.8,748.9,738.5]+3100+503).*0.453592;
+
+WeightofFuel_landing = MassOfFuel_landing.*9.81;
+CL_landing = WeightofFuel_landing./(0.5.*rho.*IAS_landing.^2.*17.48);
+AoA_landing = [-5.547,-4.593,-3.096,-1.731,1.525,4.986];
+
+hold on
+scatter(AoA_landing,CL_landing,'*');
+p2 = polyfit(AoA_landing,CL_landing,1);
+hold on
+plot(range,p2(1).*range+p2(2));
+
+legend('points for zero Xcg','line for zero Xg','points for Xcg-5','line for Xcg-5','points for Xcg+5','line for Xcg+5');
+xlabel('Angel of attack in degree');
+ylabel('CL(Coefficient of life)');
+title('CL vs AoA for landing setup');
+hold off
