efbff68e12dd81d0e05c18ffc979ae8bfea63365
SCARLETRAIN511 <992127134@qq.com>
2020-05-27 23:57:44 +0800

    1

--- Year2/numerical analysis/MofF.m
+++ Year2/numerical analysis/MofF.m
@@ -1,7 +1,11 @@
 %%solve the stability quintic equation
-result = solveEqn(0,0,0,1.31,0.035,0.024,0);
-disp(simplify(result));
-fprintf("The result is %3.6f",result(1));
+clear
+clc
+
+result = solveEqn(0,1,1.804,1.31,0.035,0.024,0);
+
+fprintf("The real part of the result is %3.6f \n",result(4));
+fprintf("The imaginary part of the result is %3.6fi",imag(result(4)));
 function result = solveEqn(a,b,c,d,e,f,g)
     syms x y;
     result = solve(a*x^5+b*x^4+c*x^3+d*x^2+e*x+f==g,x);
