b1a140e923f16ba3dda90b7309fe2ce94c5844bc
SCARLETRAIN511 <992127134@qq.com>
2020-06-05 02:28:52 +0100

    Update Part1c.m

--- nacw2/Part1c.m
+++ nacw2/Part1c.m
@@ -6,9 +6,12 @@ b=0.5;
 q=1;
 r=1;
 s=10;
+%starts from T which gives the reversed order;from T to 0
 T=20;
+%time step = 0.25
 h=0.25;
 n=T/h;
+%initial guess
 P(1)=s;
 for i=1:n
     P(i+1)=P(i)+h*(2*P(i)+q-(b^2/r)*(P(i))^2);
@@ -24,20 +27,21 @@ s=10;
 T=20;
 h=0.25;
 n=T/h;
-P2(1)=s;
+pRK(1)=s;
 for i=1:n
-    k1=h*(2*P2(i)+q-(b^2/r)*(P2(i))^2);
-    k2=h*(2*(P2(i)+1/2*k1)+q-(b^2/r)*(P2(i)+1/2*k1)^2);
-    k3=h*(2*(P2(i)+1/2*k2)+q-(b^2/r)*(P2(i)+1/2*k2)^2);
-    k4=h*(2*(P2(i)+k3)+q-(b^2/r)*(P2(i)+k3)^2);
-    P2(i+1)=P2(i)+(1/6)*k1+(1/3)*(k2+k3)+(1/6)*k4;
+    k1=h*(2*pRK(i)+q-(b^2/r)*(pRK(i))^2);
+    k2=h*(2*(pRK(i)+1/2*k1)+q-(b^2/r)*(pRK(i)+1/2*k1)^2);
+    k3=h*(2*(pRK(i)+1/2*k2)+q-(b^2/r)*(pRK(i)+1/2*k2)^2);
+    k4=h*(2*(pRK(i)+k3)+q-(b^2/r)*(pRK(i)+k3)^2);
+    pRK(i+1)=pRK(i)+(1/6)*k1+(1/3)*(k2+k3)+(1/6)*k4;
     
 end
-P2=flip(P2);
+pRK=flip(pRK);
 
-plot([0:80]/4,P,'rx-')
+time = [0:0.25:20];
+plot(time,P,'rx-');
 hold on
-plot([0:80]/4,P2,'bo-')
+plot(time,pRK,'bo-');
 xlabel('t/s');
 ylabel('p(t)');
 title('Numerical solutions to the ODE')
@@ -46,11 +50,11 @@ legend('Explicit Euler',...
 
 
 figure
-X(1)=5
+X(1)=5;
 for i=1:n
-    X(i+1)=X(i)+h*a*X(i)-((b^2)/r)*h*P2(i)*X(i)
+    X(i+1)=X(i)+h*a*X(i)-((b^2)/r)*h*pRK(i)*X(i);
 end
-plot([0:80]/4,X,'bx-')
+plot(time,X,'bx-')
 xlabel('time/s');
 ylabel('x(t)');
 title('The explicit solution to the ODE')
