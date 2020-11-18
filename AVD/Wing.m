11471fe0b1e80d79a97682bc9b297c3f8125e69c
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-11-18 23:24:32 +0800

    Update Wing.m

--- AVD/Wing.m
+++ AVD/Wing.m
@@ -11,3 +11,10 @@ lambda = 0.29;
 AR = 9.4;
 SLE2 = tan(SQC/180*pi) + ((1-lambda)/(AR*(1+lambda)));
 disp(atan(SLE2)/pi*180);
+
+%% sf
+sf = 0.907;
+Length = 4.9605 * (sf)^0.4;
+Diameter = 3.49 * (sf)^0.5;
+Weight = 6147.1 * (sf)^1.1;
+
