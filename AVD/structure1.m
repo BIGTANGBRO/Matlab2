b6fca2f18ead01e2faa5e8759b6353ad5a6b33d4
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-11-20 16:59:33 +0800

    Create structure1.m

---
+++ AVD/structure1.m
@@ -0,0 +1,11 @@
+%% structure lab
+
+shearModulus = 28e9;%pa
+Vin = 15;%v
+Gain = 1000;%gain
+s = 2.09;
+load = input("Input the voltage when loading");
+unload = input("Input the voltage when unloading");
+Vout = load - unload;
+q = input("input the shear flow");
+
