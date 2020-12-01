561bec519824c7295c2415b4b3be9d69107aafd2
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-02 01:05:48 +0800

    update

--- CFD/CFDq1d.m
+++ CFD/CFDq1d.m
@@ -1,25 +1,68 @@
 clc
 clear
+%assume lambda = 1, m = 3
 
-deltaX = 1/10;
+deltaX = 1/500;
 mdx = 0:deltaX:1;
 %number of points
 N = 1./deltaX + 1;
 
+%abc for ui+1,ui,ui-1;
 aCoeff = 1;
-bCoeff = -(2+deltaX.^2);
+bCoeff = -2-deltaX.^2;
 cCoeff = 1;
+
 a = zeros(N-2,1);
 c = zeros(N-2,1);
-a(2:N-2) = aCoeff;
-c(1:N-3) = cCoeff;
+a(1:N-2) = aCoeff;
+c(1:N-2) = cCoeff;
 b = ones(N-2,1)*bCoeff;
 
-fMatrix = deltaX.^2*(-9*pi^2-1).*sin(3.*pi.*mdx(2:N-1));
+%matrix for m
+fMatrix(:,1) = deltaX.^2*(-9*pi^2-1).*sin(3.*pi.*mdx(2:N-1));
+% 
+% beta = zeros(N-2,1);
+% gamma = zeros(N-2,1);
+% 
+% %forward
+% beta(1) = b(1);
+% gamma(1) = fMatrix(1)./beta(1);
+% for k = 2:N-2
+%     beta(k) = b(k)-a(k)*c(k-1)/beta(k-1);
+%     gamma(k) = (-a(k)*gamma(k-1)+fMatrix(k))/beta(k);
+% end
+%intergrate using function to represent forward algorithms
+[gamma,beta] = forward(a,b,c,fMatrix,N);
 
+%backward
+%last 2 is zero
+u = zeros(N-2,1);
+u(N-2) = gamma(N-2);
+for k = (N-3):-1:1
+    u(k) = gamma(k) - u(k+1)*c(k)/beta(k);
+end
+u = [0;u;0];
+
+%plot
+plot(mdx,u,'--r');
+hold on 
+plot(mdx,sin(3.*pi.*mdx),'-g');
+grid on
+legend('Predicted','Actual');
+hold off
+
+%calculate the theoretical values
+ubar = transpose(sin(3*pi*mdx));
+epsilon = max(abs(u-ubar));
+fprintf('The maximum epsilon of direct is %3.5f',epsilon);
+
+
+%% functions
+function [gamma,beta] = forward(a,b,c,fMatrix,N)
 beta = zeros(N-2,1);
 gamma = zeros(N-2,1);
 
+%forward
 beta(1) = b(1);
 gamma(1) = fMatrix(1)./beta(1);
 for k = 2:N-2
@@ -27,14 +70,5 @@ for k = 2:N-2
     gamma(k) = (-a(k)*gamma(k-1)+fMatrix(k))/beta(k);
 end
 
-u = zeros(N,1);
-u(N-2) = gamma(N-2);
-for k = (N-3):-1:1
-    u(k) = gamma(k) - u(k+1)*c(k)/beta(k);
 end
-    
-ubar = transpose(sin(3*pi*mdx));
-eps = max(u-ubar);
-disp(eps);
-
 
--- CFD/CFDq1e.m
+++ CFD/CFDq1e.m
@@ -1,44 +1,48 @@
 clear
 clc
 
+%assume lambda=1,m=3;
 lambda = 1;
 m = 3;
 %differen intervals
-dX = [1/10,1/20,1/50,1/100,1/200,1/1000];
+deltaX = 1/500;
 %calculate Numnber of mesh points
-N = 1./dX + 1;
+N = 1./deltaX + 1;
+mdx = [0:deltaX:1];
 
-%for each dx 
-for i = 1:length(dX)
-    mdx = [0:dX(i):1];
-    b = transpose((dX(i).^2).*(m.^2*pi.^2 - lambda)*sin(m*pi*mdx(2:length(mdx)-1)));
-    
-    %initialize matrix A
-    A = zeros(N(i)-2,N(i)-2);
-    A(1:N(i)-2,1:N(i)-2) = -(2+lambda*dX(i)^2);
-    A(1:N(i)-3,2:N(i)-2) = 1;
-    A(1:N(i)-2,1:N(i)-3) = 1;
-    
-    %set the initial condition
-    x(1:N(i)-2,1) = 0;
-    r(1:N(i)-2,1) = b - A*x(1:N(i)-2,1);
-    
-    eps = 1;
-    k = 1;%actually starts from 0
-    
-    while eps >= 10^-7
-        alpha = transpose(r(:,k)) * r(:,k)/(transpose(r(:,k))*(A*r(:,k)));
-        x(:,k+1) = x(:,k) + alpha*r(:,k);
-        r(:,k+1) = r(:,k) - alpha*A*r(:,k);
-        eps = sqrt(1/N(i)*sum(r(:,k).^2));
-        k = k + 1;
-    end
-    
-    x = transpose(x(:,k));
-    xbar = sin(3*pi*mdx(2:length(mdx)-1));
-    eMax(i) = max(abs(x-xbar));
-    
-    plot(mdx(2:length(mdx)-1),x)
-    hold on
-    
+%calculate B matrix
+b = transpose(deltaX^2.*(-1-m^2*pi*2)*sin(3*pi*mdx(2:N-1)));
+
+%calculate A matrix
+A = zeros(N-2,N-2);
+for i = 1:N-2
+    A(i,i) = (-2-lambda*deltaX^2);
 end
+for i = 2:N-2
+    A(i,i-1) = 1;
+    A(i-1,i) = 1;
+end
+
+u = zeros(N-2,1);
+eps = 3;
+n=1;
+while eps >= 10^(-7)
+    r(:,n) = b - A*u(:,n);
+    alpha = (transpose(r(:,n))*r(:,n))/(transpose(r(:,n))*A*r(:,n));
+    u(:,n+1) = u(:,n) + alpha * r(:,n);
+    eps = sqrt((1/N)*sum(r(:,n).^2));
+    n = n + 1;
+end
+
+ubar = transpose(sin(3*pi*mdx));
+ufinal = [0;u(:,length(u(1,:)));0];
+epsilon = max(abs(ufinal-ubar));
+fprintf('The maximum epsilon of iteration is %3.5f',epsilon);
+
+plot(mdx,ufinal,'--r');
+%predicted values;
+hold on 
+plot(mdx,sin(3.*pi.*mdx),'-g');
+grid on
+legend('Predicted','Actual');
+hold off
