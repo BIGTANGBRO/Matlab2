aaaf5c71dbd5476d1598ae731854eb7969dfad5a
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-12-02 14:50:21 +0800

    update

--- CFD/CFDcw2.m
+++ CFD/CFDcw2.m
@@ -6,7 +6,7 @@ lamda = 1; %default value is 1
 m = 3 ;
 a = 0; %B.C.
 b = 0; %B.C.
-delta_x = [1/10, 1/20, 1/50, 1/100, 1/200, 1/1000]; %length of mesh interval
+delta_x = [1/10, 1/20, 1/100, 1/100, 1/200, 1/1000]; %length of mesh interval
 
 N = (1./delta_x)+1; %number of mesh points
 
@@ -14,32 +14,44 @@ for time = 1:length(delta_x)
     x = 0 : delta_x(time) : 1;
     b = (delta_x(time)^2)*(-((m*pi)^2) - lamda)*sin(m*pi*x(2:length(x) - 1));
     b = transpose(b);
-    A = zeros(N(time) - 2, N(time) - 2); %create a nil matrix
-    A(1:N(time) - 2,1:N(time) - 2) = -(2+lamda*(delta_x(time)^2));
-    A(1:N(time) - 3,2:N(time) - 2) = 1;
-    A(2:N(time) - 2,1:N(time) - 3) = 1;
+    clear vars A
+    A = zeros(N(time) - 2, N(time) - 2); %create a nil matrix for A
+    for i = 1:N(time) - 2
+        A(i,i) = -(2+lamda*(delta_x(time)^2));
+    end
+    for j = 1:N(time) - 3
+        A(j,j+1) = 1;
+        A(j+1,j) = 1;
+    end
+    %A(1:N(time) - 2,1:N(time) - 2) = -(2+lamda*(delta_x(time)^2));
+    %A(1:N(time) - 3,2:N(time) - 2) = 1;
+    %A(2:N(time) - 2,1:N(time) - 3) = 1;
     u(1:N(time) - 2,1) = 0;
+    
+    clear vars r
     eps = 1;
-    n = 1; %iteration time
+    n = 0; %iteration time
     while eps >= 10^(-7)
+        n = n + 1;
         r(:,n) = b - A * u(:,n);
+        eps = sqrt((1/N(time))*sum(r(:,n).^2));
         alpha = transpose(r(:,n))*(b - A*u(:,n))/(transpose(r(:,n))*A*r(:,n));
         u(:,n+1) = u(:,n) + alpha * r(:,n);
-        eps = sqrt((1/N(time))*sum(r(:,n).^2));
-        n = n + 1;
+        
     end
     
     u = transpose(u(:,n)); 
     
-    u_bar = sin(3*pi*x(2:length(x) - 1)); %exact solution
+    u_bar = sin(m*pi*x(2:length(x) - 1)); %exact solution
     
     error_max(time) = max(abs(u - u_bar));
     
-    clear vars r
+    
     
     figure(time)
-    plot(x(2:length(x)-1),u)
+    plot(x(2:length(x)-1),u,'-r')
     hold on
-    plot(x(2:length(x)-1),u_bar)
+    plot(x(2:length(x)-1),u_bar,'--b')
+    hold off
     
 end
--- CFD/CFDq1e.m
+++ CFD/CFDq1e.m
@@ -1,17 +1,21 @@
 clear
 clc
 
+deltaXMatrix = [1/10,1/20,1/50,1/100,1/200,1/500];
+for x = 1:length(deltaXMatrix)
 %assume lambda=1,m=3;
 lambda = 1;
 m = 3;
 %differen intervals
-deltaX = 1/500;
+deltaX = deltaXMatrix(x);
+
+clear var b eps u r alpha
 %calculate Numnber of mesh points
 N = 1./deltaX + 1;
 mdx = [0:deltaX:1];
 
 %calculate B matrix
-b = transpose(deltaX^2.*(-1-m^2*pi*2)*sin(3*pi*mdx(2:N-1)));
+b = transpose(deltaX^2.*(-1-m^2*pi^2)*sin(3*pi*mdx(2:N-1)));
 
 %calculate A matrix
 A = zeros(N-2,N-2);
@@ -24,21 +28,22 @@ for i = 2:N-2
 end
 
 u = zeros(N-2,1);
-eps = 3;
-n=1;
+eps = 1;
+n=0;
 while eps >= 10^(-7)
+    n = n + 1;
     r(:,n) = b - A*u(:,n);
     alpha = (transpose(r(:,n))*r(:,n))/(transpose(r(:,n))*A*r(:,n));
-    u(:,n+1) = u(:,n) + alpha * r(:,n);
     eps = sqrt((1/N)*sum(r(:,n).^2));
-    n = n + 1;
+    u(:,n+1) = u(:,n) + alpha * r(:,n);
 end
 
 ubar = transpose(sin(3*pi*mdx));
 ufinal = [0;u(:,length(u(1,:)));0];
 epsilon = max(abs(ufinal-ubar));
-fprintf('The maximum epsilon of iteration is %3.5f',epsilon);
+fprintf('The maximum epsilon of iteration is %3.5f\n',epsilon);
 
+figure()
 plot(mdx,ufinal,'--r');
 %predicted values;
 hold on 
@@ -46,3 +51,4 @@ plot(mdx,sin(3.*pi.*mdx),'-g');
 grid on
 legend('Predicted','Actual');
 hold off
+end
