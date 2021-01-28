18cb5530d54f708763e0f4ed5e1e2558b5bb423e
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-01-29 01:52:04 +0800

    update

---
+++ CFD_CW2/CFDCW.m
@@ -0,0 +1,79 @@
+clear
+clc
+
+%Numerical solution of the Riemann shock tube problem using Flux 
+%Vector Splitting method
+
+%Input flow variables
+gamma=1.4;
+
+%Meshes number 100/200/300
+%N=[100 200 300];
+N=100;
+dx=(2--2)./(N-1); %Calculating different values of delta x
+x=linspace(-2,2,N(1)); %Equally spaced point in the interval -2<x<2
+dt=0.005; %Time interval
+t=0:dt:0.5;
+
+%Initial conditions
+pr=10; %Pressure ratio p2/p1
+dr=8; %Density ratio
+%Driven part (1) - x<0
+%Assuming p1=1 and rho1=1 ??
+rho=ones(1,N);
+p=ones(1,N);
+u=zeros(1,N);
+%Driven part (2) - x>0
+rho(1:N/2)=dr;
+p(1:N/2)=pr;
+E=p./(rho.*(gamma-1))+0.5.*u.^2;
+U=[rho;rho.*u;rho.*E];
+
+for n=1:length(t)
+    
+    for i=1:N
+        %Constructing Jacobian matrix A
+        A=Jacob(u(i),E(i));
+        %Calculating the eigenvalues and sort in asending order
+        lambda=sort(eig(A));
+        c(i)=lambda(3)-lambda(2);
+        
+        H=E+p./rho; %Total entropy
+        
+        %Splitting of the eigenvalues and the fluxes
+        lp=0.5.*(lambda+abs(lambda));
+        lm=0.5.*(lambda-abs(lambda));
+        Fp(:,i)=0.5*rho(i)/gamma.*[lp(1)+2*(gamma-1)*lp(2)+lp(3);lambda(1)*lp(1)+2*(gamma-1)*u(i)*lp(2)+lambda(3)*lp(3);(H(i)-u(i)*c(i))*lp(1)+(gamma-1)*u(i)^2*lp(2)+(H(i)+u(i)*c(i))*lp(3)];
+        Fm(:,i)=0.5*rho(i)/gamma.*[lm(1)+2*(gamma-1)*lm(2)+lm(3);lambda(1)*lm(1)+2*(gamma-1)*u(i)*lm(2)+lambda(3)*lm(3);(H(i)-u(i)*c(i))*lm(1)+(gamma-1)*u(i)^2*lm(2)+(H(i)+u(i)*c(i))*lm(3)];
+    end
+    
+    %Calculating U by using first order upwind scheme in Qusetion (2)
+    for j=2:N-1
+        U(:,j)=U(:,j)-dt/dx*((Fp(:,j)-Fp(:,j-1))+(Fm(:,j+1)-Fm(:,j)));
+    end
+    
+    %Applying boundary conditions U(0)=U(1)/U(N+1)=U(N)
+    U(:,1)=U(:,2);
+    U(:,N)=U(:,N-1);
+    
+    %Updating the variables for next time iteration
+    rho(:)=U(1,:);
+    E(:)=U(3,:)./U(1,:);
+    u(:)=U(2,:)./U(1,:);
+    p(:)=(gamma-1).*rho.*(E-0.5.*u.^2);
+    
+end
+
+plot(x,p,'LineWidth',1.5)
+grid minor
+
+
+
+function J = Jacob(u,E)
+
+gamma=1.4;
+J(1,:)=[0 1 0];
+J(2,:)=[0.5*(gamma-3)*u^2 (3-gamma)*u gamma-1];
+J(3,:)=[(gamma-1)*u^3-gamma*u*E gamma*E-0.5*3*(gamma-1)*u^2 gamma*u];
+
+end
--- CFD_CW2/CFD_CW2.m
+++ CFD_CW2/CFD_CW2.m
@@ -9,47 +9,52 @@ gamma = 1.4;
 xBegin = -2;
 xEnd = 2;
 %set the space
-N = 99;
+N = 300;
 
 %initial condition
 denRatio = 8;
 preRatio = 10;
 %set the deltaX and deltaT
-deltaX = 4/N;
-dX = [-2:deltaX:2];
-deltaT = 0.001;
+deltaX = 4/(N-1);
+dX = linspace(-2,2,N);
+
+deltaT = 0.005;
 dT = [0:deltaT:0.5];
 
 %initial conditions
 rho = ones(1,length(dX));
-rho(:,1:ceil(length(rho)/2)) = denRatio;
-pressure = ones(1,length(dX));
-pressure(:,1:ceil(length(pressure)/2)) = preRatio;
-u = zeros(1,length(dX));
+rho(1:N/2) = denRatio;
+pressure = ones(1,N);
+pressure(1:N/2) = preRatio;
+u = zeros(1,N);
 E=pressure./((gamma-1).*rho)+u.^2/2;
 BigU = [rho;rho.*u;rho.*E];
 
 %iteration until t = 0.5;
 for n = 1:length(dT)
-    for i = 1:length(dX)
+    for i = 1:N
         %initial value of u is 0;
         %for each dx
         mA = getMatrixA(u(i),E(i));
-%         eigValues(:,i) = sort(eig(mA)); 
-%         uvalue = eigValues(2,i);
-%         cvalue = eigValues(3,i) - eigValues(2,i);
-%         
-%         %from eig to get F+ F-
-%         %get F+;
-%         %get F-;    
-%         if (eigValues(1,i)>=0)
-%             FPlus(:,i)= [rho(i)*u(i);rho(i)*u(i)^2+pressure(i);rho(i)*u(i)*(E(i)+pressure(i)/rho(i))];
-%             FMinus(:,i)= [0;0;0];
-%         else
-%             FPlus(:,i) =rho(i)/(2*gamma)*[(2*gamma-1)*uvalue+cvalue;2*(gamma-1)*uvalue^2+(uvalue + cvalue)^2;(gamma-1)*uvalue^3+(uvalue-cvalue)^3/2+(3-gamma)/(2*(gamma-1))*(uvalue+cvalue)*cvalue^2];
-%             FMinus(:,i)= rho(i)*(uvalue-cvalue)/(2*gamma)*[1;uvalue-cvalue;(uvalue-cvalue)^2/2+(3-gamma)/(gamma-1)*cvalue^2/2];
-%         end
+        %get F+;
+        %get F-;   
+        %2 methods
+%method1
+        %get mach
+         eigValues(:,i) = sort(eig(mA)); 
+         cValue(i) = eigValues(3) - eigValues(2);
+%         uValue(i) = eigValues(2);
+%         H = E+pressure./rho;
+%          %from eig to get F+ F-
 
+%         vPlus=0.5.*(eigValues+abs(eigValues));
+%         vMinus=0.5.*(eigValues-abs(eigValues));
+%         FPlus(:,i)=0.5*rho(i)/gamma.*[vPlus(1)+2*(gamma-1)*vPlus(2)+vPlus(3);eigValues(1)*vPlus(1)+2*(gamma-1)*u(i)*vPlus(2)+eigValues(3)*vPlus(3);(H(i)-u(i)*cValue(i))*vPlus(1)+(gamma-1)*u(i)^2*vPlus(2)+(H(i)+u(i)*cValue(i))*vPlus(3)];
+%         FMinus(:,i)=0.5*rho(i)/gamma.*[vMinus(1)+2*(gamma-1)*vMinus(2)+vMinus(3);eigValues(1)*vMinus(1)+2*(gamma-1)*u(i)*vMinus(2)+eigValues(3)*vMinus(3);(H(i)-u(i)*cValue(i))*vMinus(1)+(gamma-1)*u(i)^2*vMinus(2)+(H(i)+u(i)*cValue(i))*vMinus(3)];
+%         
+
+%method 2
+        mF = getMatrixF(rho(i),u(i),pressure(i),E(i));
         [eigenVectors,eigenValues] = eig(mA);
         Vplus = 0.5.*(eigenValues + abs(eigenValues));
         Vminus = 0.5.*(eigenValues - abs(eigenValues));
@@ -60,35 +65,111 @@ for n = 1:length(dT)
     end
     
    %get the new Bigu
-    for i2 = 2:length(dX)-1 
+    for j = 2:N-1 
         %BigU n+1 = BigU n + fun(F+,F-);
-        BigU(:,i2) = BigU(:,i2) - (deltaT/deltaX).*(FPlus(:,i2)-FPlus(:,i2-1)+(FMinus(i2+1)-FMinus(i2)));
+        BigU(:,j) = BigU(:,j) - (deltaT/deltaX).*(FPlus(:,j)-FPlus(:,j-1)+FMinus(:,j+1)-FMinus(:,j));
         %update the big U
     end
     
     %get from U2 to Un-1, U2 = U1,Un = Un-1
     %get boundary conditions
     BigU(:,1) = BigU(:,2);
-    BigU(:,length(dX)) = BigU(:,length(dX)-1); 
+    BigU(:,N) = BigU(:,N-1); 
     %from BigU n +1,get new rho,v,pressure
     
-    for i3 = 1:length(dX)
-        rho(i3) = BigU(1,i3);
-        u(i3) = BigU(2,i3)/BigU(1,i3);
-        E(i3) = BigU(3,i3)/BigU(1,i3);
-        pressure(i3) = (gamma-1)*rho(i3)*(E(i3)-u(i3)^2/2);
-    end
-
+    rho(:) = BigU(1,:);
+    u(:) = BigU(2,:)./BigU(1,:);
+    E(:) = BigU(3,:)./BigU(1,:);
+    pressure(:) = (gamma-1).*rho.*(E-0.5*u.^2);
+   
 end
-plot(dX,u);
+%get entropy
+entropy=log(pressure./(rho.^gamma));
+
+%find maximum lambda to find limit of deltaT
+for i = 1:N
+    Lambda(i) = max(eigValues(:,i));
+end
+maxLambda = max(Lambda);
+maxdeltaT = deltaX/maxLambda;
+fprintf("The maximum delta T is %3.4f",maxdeltaT);
+
+%import the analytical solution
+analyticalSol = importdata('shock_analytic.dat');
+xA = analyticalSol.data(:,1);
+rhoA = analyticalSol.data(:,2);
+pA = analyticalSol.data(:,3);
+uA = analyticalSol.data(:,4);
+machA = analyticalSol.data(:,5);
+entropyA = analyticalSol.data(:,6);
+
+%plot section
+figure(1);
+plot(dX,u,'--');
+hold on
+plot(xA,uA);
+grid on
+xlabel('x');
+ylabel('Velocity');
+legend("numerical","Analytical")
+title('N = 200');
+hold off
+
+figure(2);
+plot(dX,pressure,'--');
+hold on
+plot(xA,pA);
+grid on
+xlabel('x');
+ylabel('Pressure');
+legend("numerical","Analytical")
+title('N = 200');
+hold off
+
+figure(3)
+plot(dX,rho,'--');
+hold on
+grid on
+plot(xA,rhoA);
+xlabel('x');
+ylabel('Density');
+legend("numerical","Analytical")
+title('N = 200');
+hold off
+
+figure(4)
+plot(dX,entropy,'--');
+hold on
+grid on
+plot(xA,entropyA);
+xlabel('x');
+ylabel('Entropy');
+legend("numerical","Analytical")
+title('N = 200');
+hold off
+
+figure(5)
+machNum = u./cValue;
+plot(dX,machNum,'--');
+hold on
+grid on
+plot(xA,machA);
+xlabel('x');
+ylabel('Mach number');
+legend("numerical","Analytical")
+title('N = 200');
+hold off
 
 %functions
 %matrix function handler for matrixA
 function matrixA = getMatrixA(u,E)
+    gamma = 1.4;
     matrixA = [0,1,0;
-        -1.6*u^2/2,1.6*u,0.4;
-        0.4*u^3-1.4*u*E,1.4*E-3*0.2*u^2,1.4*u];
+        0.5*(gamma-3)*u^2,(3-gamma)*u,gamma-1
+        (gamma-1)*u^3-gamma*u*E,gamma*E-0.5*3*(gamma-1)*u^2,gamma*u];
 end
 
-
+function matrixF = getMatrixF(rho,u,pressure,E)
+    matrixF= [rho*u;rho*u^2+pressure;rho*u*(E+pressure/rho)];
+end
 
