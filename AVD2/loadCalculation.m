dc2bef40b7f27369058c6ab87644b7bd532b662e
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2021-02-12 14:51:11 +0800

    Update loadCalculation.m

--- AVD2/loadCalculation.m
+++ AVD2/loadCalculation.m
@@ -1,4 +1,14 @@
 %code to calculate the load
+clear 
+clc
 
 n = 300;
 width = 26.595/n;
+croot = 6;
+c = [6:-3/(n-1):3];
+b = 53.19/2;
+loadFactor = 3.4;
+rho = 0.33;
+%moment of coefficient
+cm0 = 0.07;
+
