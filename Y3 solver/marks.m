929725af4b48937eb99f4180cef2af5f4f402a89
tangshao <tangshao@qunhemail.com>
2021-07-26 11:00:22 +0800

    Update marks.m

--- Y3 solver/marks.m
+++ Y3 solver/marks.m
@@ -4,12 +4,12 @@ clc
 fe = 67.5;
 cfd = 66.5;
 
-structure = 82;
-aero = 69;
-control = 83;
-maths =68;
+structure = 75;
+aero = 66;
+control = 78;
+maths = 62;
 avd = 62;
-gdp = 70;
+gdp = 67;
 
 coeff1 = 0.122;
 coeff2 = 0.0833;
@@ -45,4 +45,4 @@ p9 = 0.0619;
 p10 = 0.0619;
 p11=0.0929;
 p12 = 0.0929;
-mtotal = aero * p1 + cir *p2 + exp*p3 + L2*p4+MP*p5+material *p6+maths*p7+mf*p8+na*p9+pt*p10+smd*p11+business*p12
+mtotal = aero * p1 + cir *p2 + exp*p3 + L2*p4+MP*p5+material *p6+maths*p7+mf*p8+na*p9+pt*p10+smd*p11+business*p12;
